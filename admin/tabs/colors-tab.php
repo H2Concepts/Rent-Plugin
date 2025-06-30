31810ec22894c7027786d718eb92e29814465c07
H2Concepts <hello@h2concepts.de>
2025-06-30 20:49:57 +0200

    Update colors-tab.php

--- admin/tabs/colors-tab.php
+++ admin/tabs/colors-tab.php
@@ -81,6 +81,7 @@ $frame_colors = $wpdb->get_results($wpdb->prepare("SELECT * FROM $table_name WHE
     <!-- Form -->
     <div class="federwiegen-form-card">
         <form method="post" action="">
+            <?php wp_nonce_field('federwiegen_admin_action', 'federwiegen_admin_nonce'); ?>
             <?php if ($edit_item): ?>
                 <input type="hidden" name="id" value="<?php echo $edit_item->id; ?>">
                 <h4>Farbe bearbeiten</h4>
